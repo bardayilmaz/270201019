5c0dcd033c6034a116c75def7a1a28a86be3a55c
Bülent Arda Yılmaz <bardayilmaz35@gmail.com>
2020-11-16 08:57:52 +0000

    lab5

--- lab5/example1.py
+++ lab5/example1.py
@@ -1,3 +1,7 @@
-intInput = int(input("type an int: "))
+intInput = int(input("type an int between 0 and 11: "))
+while intInput <= 0 or intInput >10:
+	print("please enter a valid number!")
+	intInput = int(input("type an int between 0 and 11: "))
+
 for i in range(1, 11, 1):
 	print(intInput, "x", i, "=", + intInput*i)
