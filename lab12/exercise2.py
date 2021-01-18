163feac86665b7aedbf8907ffe0f1ed1fe153b8b
Bülent Arda Yılmaz <bardayilmaz35@gmail.com>
2021-01-18 09:24:16 +0000

    .

--- lab12/exercise2.py
+++ lab12/exercise2.py
@@ -1,18 +1,21 @@
 def binarySearch(element, nums):
+	if len(nums) == 0:
+		return -1
 
-    if len(nums) == 0: 
-        return -1
-    mid = len(nums)//2
-    item = nums[mid]
+	mid = len(nums) // 2
+	item = nums[mid]
 
-    if item == element:
-        return mid
-    elif element < item: 
+	if item == element:
+		return mid
+	elif element < item:
+		return binarySearch(element, nums[:mid])
+	else:
+		loc = binarySearch(element, nums[mid+1:])
+		if loc == -1:
+			return -1
+		return loc + mid +1				
 
-        return binarySearch(element, nums[:mid])
-    else: 
-
-        return binarySearch(element, nums[mid+1:]) + mid +1
+  
 
 a = [4, 4, 5, 7, 8, 9, 32, 66, 222, 321, 412, 3214]
-print(binarySearch(412,a))
+print(binarySearch(3121,a))
