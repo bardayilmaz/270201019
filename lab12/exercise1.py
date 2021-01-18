3dd5cb7bff8449e99bd8ab42b25f45d76f42fc8d
Bülent Arda Yılmaz <bardayilmaz35@gmail.com>
2021-01-18 08:56:41 +0000

    .

--- lab12/exercise1.py
+++ lab12/exercise1.py
@@ -1,10 +1,12 @@
 def selectionSort(l):
 	for i in range(len(l)):
-		min = l[i]
+		min = i
 		for j in range(i+1, len(l)):
-			if l[j] < min:
-				l[j], l[i] = l[i], l[j]
+			if l[j] < l[min]:
+				min = j
+		l[i], l[min] = l[min], l[i]
+	return l	
 
-a = [9,8,7,66,5,4,321,4]
-selectionSort(a)
-print(a)
+a = [9,8,3214,32,412,222,7,66,5,4,321,4]
+
+print(selectionSort(a))
